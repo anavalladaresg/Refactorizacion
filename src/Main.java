7645eeed912edc73b2339271a72f66b1b7433991
Ana <avalladaresgonzalez@danielcastelao.org>
2024-04-15 09:53:07 +0200

    method getScoresForWin implemented

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,7 @@ public class Main {
 
     /**
      * This method returns the tennis score name corresponding to the provided numeric value.
+     *
      * @param scorePlayer1 The numeric score of the player.
      * @return The tennis score name corresponding to the numeric value.
      */
@@ -50,15 +51,18 @@ public class Main {
     }
 
     /**
-     * Method to get scores for win
+     * This method determines the winning score based on the numeric scores of the two players.
+     *
+     * @param scorePlayer1 The numeric score of player 1.
+     * @param scorePlayer2 The numeric score of player 2.
+     * @return The winning score as a string. It can be "Advantage player1", "Advantage player2", "Win for player1", or "Win for player2".
      */
     private static String getScoresForWin(int scorePlayer1, int scorePlayer2) {
-        String score;
-        int scoreDifference = scorePlayer1 - scorePlayer2;
-        if (scoreDifference == 1) score = "Advantage player1";
-        else if (scoreDifference == -1) score = "Advantage player2";
-        else if (scoreDifference >= 2) score = "Win for player1";
-        else score = "Win for player2";
-        return score;
+        int minusResult = scorePlayer1 - scorePlayer2;
+        if (minusResult == 1) return ADVANTAGE_PLAYER1;
+        else if (minusResult == -1) return ADVANTAGE_PLAYER2;
+        else if (minusResult >= 2) return WIN_FOR_PLAYER1;
+        else return WIN_FOR_PLAYER2;
     }
+
 }
