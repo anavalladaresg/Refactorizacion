5d097feead5f77b6888e9a822d11cae1e78dccff
Ana <avalladaresgonzalez@danielcastelao.org>
2024-04-15 09:26:53 +0200

    code formated

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
      */
     public static String getScore(int scorePlayer1, int scorePlayer2) {
         String score = "";
-        int tempScore=0;
+        int tempScore = 0;
 
         if (scorePlayer1 == scorePlayer2) {
-            switch (scorePlayer1)
-            {
+            switch (scorePlayer1) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -24,41 +23,37 @@ public class Main {
                 default:
                     score = "Deuce";
                     break;
-
             }
-        }
-        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
-        {
-            int minusResult = scorePlayer1-scorePlayer2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = scorePlayer1;
-                else { score+="-"; tempScore = scorePlayer2;}
-                switch(tempScore)
-                {
+        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+            int minusResult = scorePlayer1 - scorePlayer2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = scorePlayer1;
+                else {
+                    score += "-";
+                    tempScore = scorePlayer2;
+                }
+                switch (tempScore) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
 
         }
-    return score;
+        return score;
     }
 }
